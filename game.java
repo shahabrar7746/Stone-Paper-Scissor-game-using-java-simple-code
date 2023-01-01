4dbae786324d718d4cec590a1847fd0c4097b3eb
Abrar Shaha <73398955+shahabrar7746@users.noreply.github.com>
2023-01-01 19:18:05 +0530

    changes made
    1. comments has been added to the code. for better understanding of code and flow of program. 
    2. extra imports has been removed.
    3. details added in game.

--- game.java
+++ game.java
@@ -1,9 +1,8 @@
 
-import java.awt.Desktop;
-import java.net.URI;
+
 import java.util.*;
 
-public class game{
+public class Main{
 
       
 		
@@ -12,23 +11,23 @@ public class game{
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	    Random rd = new Random();
+	    Random rd = new Random();//will use while assigning element for PC.
 	    String ip = "";
 	   String numbers = "012";
-	   String st = null;
+	   String st = null;// This Variable will have response of System
 
-	   int user = 0;
-	   int pc = 0;
-	   int i = 0;
+	   int user = 0;//This have Score of the User.
+	   int pc = 0;//This will have Score of the PC/System.
+	   int i = 0;//This will define number of rounds to be made for a Match.
 	   while(i<10) {
-		   System.out.println("IP");
+		   System.out.println("Type your Choise below amoung STONE/SCISSOR and PAPER");
 		   ip = sc.nextLine();
 		
 
-		   ip = ip.toUpperCase();
+		   ip = ip.toUpperCase();//This will input from user to UpperCase.
 
 		   boolean check = false;
-	   int pcip = rd.nextInt(numbers.length());
+	   int pcip = rd.nextInt(numbers.length());//Choice for the System will be asigned using this Int Varibale 
 	   switch(pcip){
 	       case 0:
 	           st = "STONE";
@@ -40,16 +39,17 @@ public class game{
 	    	   st = "SCISSOR";
 	    	   break;
 	    	   default:
+	    	   System.out.println("An error has occured. contact developer");
 	    		   st = "Nothing";
 	   }
 
-
+        //Below have test cases. On basis of this Points will be given to user or system.
 	   if(ip.equals("PAPER") && st.equals("STONE")) {
 		   check = true;
 		   if(check) {
 			   System.out.println("User won " + ip  + " PC having " + st);
 		   }else {
-			   System.out.println("PC won " + st);
+			   System.out.println("PC won with " + st);
 		   }
 	   }
 	   if(ip.equals("PAPER") && st.equals("SCISSOR")) {
@@ -57,7 +57,7 @@ public class game{
 		   if(check) {
 			   System.out.println("User won " + ip + " PC having " + st);
 		   }else {
-			   System.out.println("PC won" + st);
+			   System.out.println("PC won with " + st);
 		   }
 	   }
 	   if(ip.equals("STONE") && st.equals("PAPER")) {
@@ -65,29 +65,32 @@ public class game{
 		   if(check) {
 			   System.out.println("User won " + ip + " PC having " + st);
 		   }else {
-			   System.out.println("PC won" + st);
+			   System.out.println("PC won with " + st);
 		   }
 	   }if(ip.equals("STONE") && st.equals("SCISSOR")) {
 		   check = true;
 		   if(check) {
 			   System.out.println("User won " + ip + " PC having " + st);
 		   }else {
-			   System.out.println("PC won " + st);
+			   System.out.println("PC won with " + st);
 		   }
 	   }if(ip.equals("SCISSOR") && st.equals("STONE")) {
 		   check = false;
 		   if(check) {
 			   System.out.println("User won " + ip + " PC having " + st);
 		   }else {
-			   System.out.println("PC won " + st);
+			   System.out.println("PC won with " + st);
 		   }
 	   }if(ip.equals("SCISSOR") && st.equals("PAPER")) {
 		   check = true;
 		   if(check) {
 			   System.out.println("User won " + ip + " PC having " + st);
 		   }else {
-			   System.out.println("PC won " + st);
+			   System.out.println("PC won with " + st);
 		   }
+	   }
+	   if(!(ip.equals("SCISSOR") && st.equals("PAPER")) && !(ip.equals("SCISSOR") && st.equals("STONE")) && !(ip.equals("PAPER") && st.equals("STONE")) && !(ip.equals("PAPER") && st.equals("SCISSOR")) && !(ip.equals("STONE") && st.equals("PAPER")) && !(ip.equals("STONE") && st.equals("SCISSOR")) && !(ip.equals("SCISSOR") && st.equals("STONE"))  && !(ip.equals(st))){
+        System.out.println("Invalid Choice try again");	       
 	   }
 	   if(!ip.equals(st)) {
 		   i++;
@@ -110,7 +113,7 @@ System.out.println();
 		   System.out.println("PC won with points " + pc);
 	   }else {
 		   if(user == pc) {
-			   System.out.println("Match draw");
+			   System.out.println("Match draw\n PC with " + pc + " User with " + user);
 		   }else {
 			   System.out.println("User won with points " + user);
 		   }
